654f4295331e3826a3d4a71e00e9dd242856906c
yiyaayiy <yiyayoyayiy@gmail.com>
2025-02-01 14:31:12 -0500

    Updated graphs

--- 02_r_scripts/function_vs.R
+++ 02_r_scripts/function_vs.R
@@ -50,3 +50,53 @@ eda_EUmap <- function(data,
   
   return(p)
 }
+
+
+
+# Average annual verified emissions by activity categories
+eda_activity_emission <- function(
+    data, 
+    selected_categories, 
+    palette_name = "rtist::vanGogh1"
+    ) {
+  ggplot(
+    data = data %>%
+      filter(category %in% selected_categories) %>%
+      group_by(com_year, category) %>%
+      summarise(total = sum(
+        com_verified_sum, na.rm = TRUE) / (1e3 * sum(com_verified_sum != 0)), 
+        .groups = "drop"
+      ), 
+    aes(
+      x = com_year, 
+      y = total, 
+      color = category, 
+      linetype = category, 
+      shape = category, 
+      group = category
+    )
+  ) +
+    geom_line(size = 1) + 
+    geom_point(size = 2) +
+    labs(
+      title = paste("Average Verified Emissions by Activity Category:\n", paste(selected_categories, collapse = ", ")),
+      x = "Year",
+      y = "1000 tCO2eq",
+      color = "Activity",
+      linetype = "Activity",
+      shape = "Activity"
+    ) +
+    paletteer::scale_colour_paletteer_d(palette_name) +
+    scale_x_continuous(breaks = seq(2005, 2022, by = 2)) +
+    scale_y_continuous(breaks = pretty_breaks(n = 8)) +
+    guides(
+      color = guide_legend(title.position = "top", title.hjust = 0.5),  # Legend title on top
+      linetype = guide_legend(title.position = "top", title.hjust = 0.5),
+      shape = guide_legend(title.position = "top", title.hjust = 0.5)
+    ) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(hjust = 0.5, size = 12),
+      legend.position = "top"
+    )
+}
